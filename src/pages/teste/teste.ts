4f3217841d3613800061a497b6e1806fb354addd
Abimael Bezerra da Silva <abimael.bs@gmail.com>
2017-01-31 18:40:55 -0300

    commit adicionando melhorias

--- src/pages/teste/teste.ts
+++ src/pages/teste/teste.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams,LoadingController  } from 'ionic-angular';
+import { NavController, NavParams,LoadingController, ActionSheetController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { InAppBrowser } from 'ionic-native';
 
 @Component({
   selector: 'page-teste',
@@ -10,10 +11,14 @@ import 'rxjs/add/operator/map';
 export class TestePage {
 
   public feeds: Array<string>;
+  public noFilter: Array<any>;
+  public hasFilter: boolean = false;
+
   private url: string = "https://www.reddit.com/new.json";
+  private olderPosts: string = "https://www.reddit.com/new.json?after=";
+  private newerPosts: string = "https://www.reddit.com/new.json?before=";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public loadingCtrl: LoadingController) {
-
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public loadingCtrl: LoadingController, public actionSheetCtrl: ActionSheetController) {
     this.fetchContent();
   }
 
@@ -27,8 +32,96 @@ export class TestePage {
     this.http.get(this.url).map(res => res.json())
       .subscribe(data => {
         this.feeds = data.data.children;
+
+        this.feeds.forEach((e, i, a) => {
+          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {
+              e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+          }
+        });
+
+
         loading.dismiss();
       });
+  }
+
+  itemSelected(url): void {
+    //alert(feed.data.url);
+    let browser = new InAppBrowser(url,'_system');
+  }
+
+  doInfinite(infiniteScroll) {
+    let paramsUrl = (this.feeds.length > 0) ? this.feeds[this.feeds.length - 1].data.name : "";
+
+      this.http.get(this.olderPosts + paramsUrl).map(res => res.json())
+        .subscribe(data => {
+
+          this.feeds = this.feeds.concat(data.data.children);
+
+          this.feeds.forEach((e, i, a) => {
+            if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {
+              e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+            }
+          })
+
+          this.noFilter = this.feeds;
+          this.hasFilter = false;
+
+          infiniteScroll.complete();
+        });
+  }
+
+  doRefresh(refresher) {
+
+    let paramsUrl = this.feeds[0].data.name;
+
+    this.http.get(this.newerPosts + paramsUrl).map(res => res.json())
+      .subscribe(data => {
+
+        this.feeds = data.data.children.concat(this.feeds);
+
+        this.feeds.forEach((e, i, a) => {
+          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {
+            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+          }
+        })
+
+        this.noFilter = this.feeds;
+        this.hasFilter = false;
+
+        refresher.complete();
+      });
+  }
+
+  showFilters(): void {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Filter options:',
+      buttons: [
+        {
+          text: 'Music',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "music");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Movies',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "movies");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.feeds = this.noFilter;
+            this.hasFilter = false;
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
   }
 
 }
